35e6d743716c8d09b92f0117676d56b7643d54c4
Nicole Candiotti <nccandiotti@gmail.com>
2022-03-14 17:22:12 -0400

    commit tree ca6940ae276f40a4549a8f017d678e600ce75d46

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,10 +7,13 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
+  #bird_params is a private method listed below
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
+
+
 
   # GET /birds/:id
   def show
@@ -21,5 +24,13 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 
 end
